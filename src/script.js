902816338e055d5ca658253de2307435a76b5c8f
Sthefanny Jaiane Silveira Santos <s.jaianecel@gmail.com>
2025-02-25 22:47:47 -0300

    Project structure

---
+++ src/script.js
@@ -0,0 +1,6 @@
+import '../.env';
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/mqtt/5.10.3/mqtt.min.js" integrity="sha512-aAHZnL2a0x1+TorrKGJirDg51TAANQIynvlf7MyqPVZ65pgRyauc/Co5MKQqPIQqxVJ8U4iP6lf2iz6MYBX5zw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+
+
